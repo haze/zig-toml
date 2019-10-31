f7ad00bc7a5f727b1acc531a83794116d5d0b016
Haze Booth <isnt@haze.cool>
2019-10-31 01:41:05 -0400

    Add benchmarks

--- build.zig
+++ build.zig
@@ -2,7 +2,9 @@ const Builder = @import("std").build.Builder;
 
 pub fn build(b: *Builder) void {
     const mode = b.standardReleaseOptions();
+
     const lib = b.addStaticLibrary("zig-toml", "src/main.zig");
+
     lib.setBuildMode(mode);
     lib.install();
 
@@ -12,7 +14,13 @@ pub fn build(b: *Builder) void {
     var spec_tests = b.addTest("src/spec_tests.zig");
     spec_tests.setBuildMode(mode);
 
-    const test_step = b.step("test", "Run library tests");
+    var benchmark = b.addTest("src/bench.zig");
+    benchmark.addPackagePath("zig-bench", "zig-bench/bench.zig");
+    benchmark.setBuildMode(.ReleaseFast);
+
+    var test_step = b.step("test", "Run library tests");
     // test_step.dependOn(&main_tests.step);
     test_step.dependOn(&spec_tests.step);
+    const bench_step = b.step("bench", "Run benchmarks");
+    bench_step.dependOn(&benchmark.step);
 }
